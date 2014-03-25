fa6ed5a521bda457c5989cfc9bc97b2a96ebd196
Tom Crayford <tcrayford@googlemail.com>
2014-03-25 16:55:45 +0000

    attempt at getting the ring handler working

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [enlive  "1.1.5"]
-                 [ring "1.2.0" :exclusions [ns-tracker]]
+                 [enlive "1.1.5"]
+                 [ring "1.2.0"]
                  [compojure "1.1.5"]
-                 [info.sunng/ring-jetty9-adapter "0.3.0"]
-                 [org.apache.curator/curator-framework "2.4.1"]])
+                 [org.apache.curator/curator-framework "2.4.1"]]
+  :ring {:handler earl.web/handler}
+  :profiles {:dev {:plugins [[lein-ring  "0.8.10"]]}
+             :ring {:handler earl.web/handler}})
--- src/earl/web.clj
+++ src/earl/web.clj
@@ -1,8 +1,28 @@
 (ns earl.web
-  (:require [compjure.core :refer :all]
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [net.cgrand.enlive-html :as html]
             [earl.cluster-state :as cluster-state]))
+
+(html/deftemplate cluster-state-page "index.html"
+  [state config]
+  [:title] (html/content "lol"))
+
+(def config
+  {:earl/brand "Yeller"
+   :earl/clusters
+   #{"YellerDev"}})
 
 (defn earl-routes [client]
   (routes
-    (GET "/"
-         "web app goes here")))
+    (route/resources "/")
+    (GET "/" [& params]
+         (reduce str (cluster-state-page (cluster-state/get-state client (first (:earl/clusters config))) config)))
+
+    (GET "/:cluster-name" [& params]
+         (reduce str (cluster-state-page (cluster-state/get-state (:cluster-name params)) config)))))
+
+(defonce zk-client (cluster-state/connect-client "localhost:2181"))
+
+(def handler (handler/site (earl-routes zk-client)))
